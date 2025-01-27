707bf59f580df42b7465eb68f8061f15ec4ee558
Rimarok <romaric.ruga@mailo.fr>
2025-01-27 17:26:26 +0100

    feat : add sass + genesis & atoms integration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
 import { defineCollection, z } from "astro:content";
 
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 const pages = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/pages" }),
   schema: z.object({
     Description: z.nullable(z.string()),
     Name: z.string(),
+    Url: z.string(),
   }),
 });
 
