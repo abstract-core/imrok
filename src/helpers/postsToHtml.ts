e1cfa3de3073dbeaecdb9307325873eefd44fc73
Rimarok <romaric.ruga@mailo.fr>
2025-02-19 11:56:45 +0100

    fix : mv nebula-atoms-astro imports to nebula-atoms + rm category span in posts list on category pages + rm limit 10 on /articles page

--- src/helpers/postsToHtml.ts
+++ src/helpers/postsToHtml.ts
@@ -1,25 +1,28 @@
 import type { InferEntrySchema } from "astro:content";
-import { stringToUrl } from "nebula-atoms-astro/stringToUrl";
-import { dateToString } from "nebula-atoms-astro/dateToString";
+import { stringToUrl } from "nebula-atoms/stringToUrl";
+import { dateToString } from "nebula-atoms/dateToString";
 import { getDate } from "./getDate";
 
-export function postsToHtml(posts: { data: InferEntrySchema<"posts"> }[]) {
+export function postsToHtml(
+  posts: { data: InferEntrySchema<"posts"> }[],
+  displayCategory: boolean = true
+) {
   return `<ul class="articles">${posts
-    .slice(0, 10)
-    .map(
-      ({ data: { Name, "Type de contenu": category, slug, ...post } }) =>
-        `<li><a href="
+    .map(({ data: { Name, "Type de contenu": category, slug, ...post } }) => {
+      const date = getDate({
+        data: post as InferEntrySchema<"posts">,
+      });
+      return `<li><a href="
           /${category.slice(3).toLowerCase().replace(/é/g, "e")}/${
-          slug || stringToUrl(Name)
-        }
-        "> <span><span>${category.replace(
-          " ",
-          "&nbsp;"
-        )}</span><span>${dateToString(
-          getDate({
-            data: post as InferEntrySchema<"posts">,
-          })!
-        )}</span></span><span>${Name}</span></a></li>`
-    )
+        slug || stringToUrl(Name)
+      }
+        "> <span>${
+          displayCategory
+            ? `<span>${category.replace(/\s/g, "&nbsp;")}</span>`
+            : ""
+        }${
+        date ? `<span>${dateToString(date)}</span>` : ""
+      }</span><span>${Name}</span></a></li>`;
+    })
     .join("")}</ul>`;
 }
